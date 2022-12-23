071647c81eca609462de48a0e8e3f98f5d12e09d
Rankrish <59313760+purusr@users.noreply.github.com>
2022-12-22 19:30:12 -0800

    updated

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,41 @@
 const Main = ()=>{
     return(
-        <h3>I am from Main.</h3>
+        <div className="container-fluid bg-secondary ml-5">
+            <h2 className="text-center pa-3 mt-2">Topics</h2>
+            <div className="card">
+                <div class="card-body">
+                    <blockquote className="blockquote mb-0">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    </blockquote>
+                    <button className="btn"> 123<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></button>
+                    <button className="btn">1234<i class="fa fa-thumbs-up fa-2x" aria-hidden="true"></i></button>
+                </div>
+            </div>
+            <br/>    
+            <div className="card">
+                <div class="card-body">
+                    <blockquote className="blockquote mb-0">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    </blockquote>
+                    <button className="btn"> 123<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></button>
+                    <button className="btn">1234<i class="fa fa-thumbs-up fa-2x" aria-hidden="true"></i></button>
+                </div>
+            </div>
+            <br/>    
+            <div className="card">
+                <div class="card-body">
+                    <blockquote className="blockquote mb-0">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    </blockquote>
+                    <button className="btn"> 123<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></button>
+                    <button className="btn">1234<i class="fa fa-thumbs-up fa-2x" aria-hidden="true"></i></button>
+                </div>
+            </div>
+            <br/>    
+        </div>
     )
 }
 
