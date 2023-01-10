ef62d4f76ad79d04685f1d32ca8cc253816fdaba
Rankrish <59313760+purusr@users.noreply.github.com>
2023-01-10 08:55:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Answers from './Answers';
 import Questions from './Questions';
 import Signin from './Signin';
 import Signup from './Signup';
+import Createquestion from './Createquestion';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Route path='/yourquestions' element={<Questions />} />
       <Route path='/signin' element={<Signin />} />
       <Route path='/signup' element={<Signup />} />
+      <Route path='/question' element={<Createquestion />} />
       <Route path='*' element={<h2 className='text-center'>404 page not found</h2>} />
     </Routes>
     </BrowserRouter>
