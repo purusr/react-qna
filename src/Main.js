5bb80262c736e492e8bb624b529dbfb5a516241c
Rankrish <59313760+purusr@users.noreply.github.com>
2023-01-19 09:20:28 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,39 +1,29 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 const Main = ()=>{
+const [question, setQuestion] = useState()
+
+useEffect(()=>{
+    axios.get('http://localhost:5000/getquestions').then((res) =>setQuestion(res.data.data))
+},[])
+
     return(
         <div className="container-fluid bg-secondary ml-5">
             <h2 className="text-center pa-3 mt-2">Topics</h2>
+           {question && question.map((d) =>
             <div className="card">
                 <div class="card-body">
                     <blockquote className="blockquote mb-0">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                    <p>{d.question}</p>
+                    <footer className="blockquote-footer">{d.userId} <cite title="Source Title"></cite></footer>
                     </blockquote>
-                    <button className="btn"> 123<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></button>
+                    <button className="btn">{d.answers.length}<i class="fa fa-comments fa-2x" aria-hidden="true"></i></button>
                     <button className="btn">1234<i class="fa fa-thumbs-up fa-2x" aria-hidden="true"></i></button>
                 </div>
             </div>
+)}
             <br/>    
-            <div className="card">
-                <div class="card-body">
-                    <blockquote className="blockquote mb-0">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                    </blockquote>
-                    <button className="btn"> 123<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></button>
-                    <button className="btn">1234<i class="fa fa-thumbs-up fa-2x" aria-hidden="true"></i></button>
-                </div>
-            </div>
-            <br/>    
-            <div className="card">
-                <div class="card-body">
-                    <blockquote className="blockquote mb-0">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                    </blockquote>
-                    <button className="btn"> 123<i class="fa fa-comments-o fa-2x" aria-hidden="true"></i></button>
-                    <button className="btn">1234<i class="fa fa-thumbs-up fa-2x" aria-hidden="true"></i></button>
-                </div>
-            </div>
+            
             <br/>    
         </div>
     )
